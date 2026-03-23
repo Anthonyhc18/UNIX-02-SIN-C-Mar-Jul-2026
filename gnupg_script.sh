ad9bcbcde501d5bda3a7c75c818f7ed8a8cd09ff
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-03-23 15:25:01 +0000

    Cifra el documento

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -12,3 +12,5 @@ echo "Hola">doc_no_cifrado.txt
 cat doc_no_cifrado.txt
 #Se usa el hash para cifrar el documento 
 gpg --output doc_cifrado.txt --encrypt --recipient  9E04700B2ACCDCDBC4B3794D981D97E977939DE5 doc_no_cifrado.txt
+#Cifra el documento 
+cat doc_cifrado.txt
