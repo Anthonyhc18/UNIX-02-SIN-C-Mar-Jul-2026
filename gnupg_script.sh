8c18c7eaa5dfbb39fe8ef6525a151becaee23ec3
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-03-23 15:14:02 +0000

    Visualiza el mensaje

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -6,3 +6,7 @@ gpg --armor --export-secret-keys
 gpg --import Llave_publica_Mateo.asc
 #Sale el correo de mi compañero
 gpg --list-keys
+#Se manda un archivo de mensaje 
+echo "Hola">doc_no_cifrado.txt
+#Se visualiza el mensaje 
+cat doc_no_cifrado.txt
