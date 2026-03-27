bf4bc637c3278a24ddd0ba905067ef1a0b060f3c
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-03-27 15:57:38 +0000

    final commit

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -10,5 +10,12 @@ ls
 #Show cpu informations
 cat cpuinfo
 #PrintWorkingDirectory 
+pwd 
 #Permite ver el id 
 ls -i
+#para crear un archivo con un mensaje 
+echo "Hola" > test.txt
+#para visualizar el mensaje 
+cat test.txt
+#muestra el estado de como se guarda un archivo 
+stat  test.txt
