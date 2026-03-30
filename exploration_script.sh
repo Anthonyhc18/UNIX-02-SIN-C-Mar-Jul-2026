2b3d0aea76ffee8e9d3c910306b432c4be2101b7
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-03-30 15:22:55 +0000

    disks and partitions

--- exploration_script.sh
+++ exploration_script.sh
@@ -1,8 +1,12 @@
 # a command used in Debian-based Linux systems (like Ubuntu and Mint) to refresh the local database of available software packages and their versions from configured repositories
 sudo apt update
-#ommand is used in Debian-based Linux distributions (like Ubuntu, Linux Mint, and Kali) to install the latest versions of all packages currently installed on your system
+#command is used in Debian-based Linux distributions (like Ubuntu, Linux Mint, and Kali) to install the latest versions of all packages currently installed on your system
 sudo apt upgrate
 #installs the GNU Parted utility on Debian-based Linux distributions like Ubuntu, Linux Mint, and Kali Linux
 sudo apt install parted 
 #Lists disks and partitions, displays their file systems, and adds visual separators to organize information.
-sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "?n--\n"
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n--\n"
+#To view the first disk partition
+parted -l
+#to viwe the second disk partition in order 
+lsblk -f
