81a3b256c055a963091c0385900f0b2f754b08fd
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-03-30 15:04:39 +0000

    Install Parted

---
+++ exploration_script.sh
@@ -0,0 +1,6 @@
+# a command used in Debian-based Linux systems (like Ubuntu and Mint) to refresh the local database of available software packages and their versions from configured repositories
+sudo apt update
+#ommand is used in Debian-based Linux distributions (like Ubuntu, Linux Mint, and Kali) to install the latest versions of all packages currently installed on your system
+sudo apt upgrate
+#installs the GNU Parted utility on Debian-based Linux distributions like Ubuntu, Linux Mint, and Kali Linux
+sudo apt install parted 
