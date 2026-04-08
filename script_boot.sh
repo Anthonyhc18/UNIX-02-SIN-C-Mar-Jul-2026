00ed91b1d86e0a9af05b570e1f60bb9bf75fa5e1
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-04-08 21:19:32 +0000

    commands to view ls files

--- script_boot.sh
+++ script_boot.sh
@@ -1,4 +1,12 @@
-#Ruta alternativa 
+#Alternative route
 cd calculadora 
-#ruta absoluta 
+#absolute path
 cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/Calculadora 
+#Create a file from the terminal
+touch script_boot.sh
+#Create a folder from the terminal
+mkdir Proyecto 
+#ls -lai, represents L following links, A allows us to see all hidden files being a hard link and I the inode number
+ls -lai
+#They are the same options but separate
+ls -l -a -i
