7a9032ce04d28db1d4467924d8ea71d029201c1a
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-04-08 21:33:37 +0000

    displays information about the current directory

--- script_boot.sh
+++ script_boot.sh
@@ -10,3 +10,14 @@ mkdir Proyecto
 ls -lai
 #They are the same options but separate
 ls -l -a -i
+#displays information about the current directory
+#Devece displays the driver type, the specific device number; if the main number is "0" it means you are doing it in a virtual machine, if it is "7" it means you are using a physical disk
+stat .
+  File: .
+  Size: 4096            Blocks: 8          IO Block: 4096   directory
+Device: 7,7     Inode: 1573125     Links: 2
+Access: (0777/drwxrwxrwx)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+Access: 2026-04-08 21:08:23.672625654 +0000
+Modify: 2026-04-08 21:08:22.477625739 +0000
+Change: 2026-04-08 21:08:22.477625739 +0000
+ Birth: 2026-04-08 21:08:22.477625739 +0000
