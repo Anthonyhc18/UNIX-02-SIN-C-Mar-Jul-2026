489b8c845ddf4d030f43ab907f8d1a0d1e560116
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-04-10 14:28:43 +0000

    Install

---
+++ Proyecto/script_distro.sh
@@ -0,0 +1,6 @@
+#These packages are used to compile code (gcc, make), configure menus (ncurses), and package the system (cpio, dosfstools).
+sudo apt update && sudo apt install -y git vim make gcc libncurses-dev flex bison bc \
+cpio libelf-dev libssl-dev syslinux dosfstools qemu-system-x86
+#Download the source code
+git clone --depth 1 https://github.com/torvalds/linux.git
+cd linux
