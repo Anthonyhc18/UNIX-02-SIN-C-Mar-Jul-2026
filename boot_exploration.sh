36041ceaf68ffae7d4f61bb5fb8fd5cc1b43e7d4
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-04-17 14:33:42 +0000

    wip:dir

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -8,3 +8,7 @@ ls -l -a -h
 ls -l -ah 
 #It is used to view files in lists, whether secret or not
 ls -lah 
+#It's used to create a folder; you put a double -- because it's an option, not a command.
+mkdir -- -rf
+#Delete a folder named -rf, as it is used to delete empty directories.
+rmdir -- -rf
