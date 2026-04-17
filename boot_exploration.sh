8f129ed17c9a927e96bec3e17a7907379ef2a29e
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-04-17 14:44:18 +0000

    wip:depth

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -16,3 +16,7 @@ rmdir -- -rf
 ls --help
 #It is used to view a manual of the file list
 man ls
+#We accessed the depth using the command `man git-clone`, pressing the / to search, we searched for `depth` and with `n` to go forward and `N` to go backward we searched for the following:
+--depth <depth>
+           Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches. If you want to clone submodules shallowly, also
+           pass --shallow-submodules.
