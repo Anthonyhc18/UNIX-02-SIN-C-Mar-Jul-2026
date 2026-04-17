c7aa2731cbd39b5caa6934e586370a57db1c9c0c
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-04-17 14:51:55 +0000

    wip:rw

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -20,3 +20,8 @@ man ls
 --depth <depth>
            Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches. If you want to clone submodules shallowly, also
            pass --shallow-submodules.
+#r--- permissions for others, read-only
+#r-x permissions for the group, read and execute only
+#rwx permissions for the user, read, write, execute
+#- file type, normal (does not allow execution)
+-rw-rw-rw
