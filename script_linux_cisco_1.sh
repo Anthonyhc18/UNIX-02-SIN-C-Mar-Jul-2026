92082c41ea3ee9b83af869d312fa396b7a88ae6b
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-05-03 20:57:39 +0000

    wip:dd

---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,4 @@
+#It is used to list the files and directories within the folder you are currently in.
+ls
+#This command is used to create a file of a specific size filled with binary zeros. It is the usual first step in creating a swap file or an empty data container.
+dd if=/dev/zero of=/tmp/swapex bs=1M count=50
