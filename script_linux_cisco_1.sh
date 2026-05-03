17226aacbe02c35d70011ce48cb3dc56a74f7af7
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-05-03 21:18:05 +0000

    wip:Level2

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -2,4 +2,9 @@
 # This module deals exclusively with the CLI, or command-line interface, rather than the GUI, or graphical user interface, with which you may be more familiar.
 #It is used to list the files and directories within the folder you are currently in.
 ls
-
+#Level 2: Arguments
+#An argument can be used to specify what the command should act upon. If the `ls` command is given the name of a directory as an argument, the result will be a list of the contents of that directory.
+#It is used to specifically check if that file exists in your current directory.
+Ls script_linux_cisco_1.sh
+#It's an "Easter Egg" or hidden joke within the aptitude package manager. Its sole function is to engage in a humorous dialogue with the user that culminates in an ASCII art drawing of a cow.
+aptitude moo
