3fedfc0e4d60bd91587cf1d9d0652f5e37fbc6c4
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-05-03 21:09:50 +0000

    Wip:level1

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,4 +1,5 @@
+#Level 1: Basic command syntax
+# This module deals exclusively with the CLI, or command-line interface, rather than the GUI, or graphical user interface, with which you may be more familiar.
 #It is used to list the files and directories within the folder you are currently in.
 ls
-#This command is used to create a file of a specific size filled with binary zeros. It is the usual first step in creating a swap file or an empty data container.
-dd if=/dev/zero of=/tmp/swapex bs=1M count=50
+
