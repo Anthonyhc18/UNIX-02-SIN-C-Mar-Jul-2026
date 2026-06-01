8fe309b13f98d7456c80e0436a0ecf0fb5dec702
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-06-01 14:45:16 +0000

    environment variables

---
+++ blackhatbash1.sh
@@ -0,0 +1,6 @@
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ bash --version
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ env
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${SHELL}
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${RANDOM}
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${UID}
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${OSTYPE}
