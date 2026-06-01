cdeb671bb427f4cca291d065cf84292d804bd942
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-06-01 15:11:47 +0000

    commands

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #!/usr/bin/env bash
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ bash --version
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ env
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${SHELL}
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${RANDOM}
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${UID}
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${OSTYPE}
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ ps -e -f
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ ps -ef
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ df --human-readable
-[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ df -h
-#[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ bash -x blackhatbash1.sh
-#[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 💀COJUDO💀✗ 127 $ bash -r blackhatbash1.sh
+bash --version
+env
+echo ${SHELL}
+echo ${RANDOM}
+echo ${UID}
+echo ${OSTYPE}
+ps -e -f
+ps -ef
+df --human-readable
+df -h
+# bash -x blackhatbash1.sh
+# 127 $ bash -r blackhatbash1.sh
+#chmod u+x blackhatbash1.sh
+#./blackhatbash1.sh
