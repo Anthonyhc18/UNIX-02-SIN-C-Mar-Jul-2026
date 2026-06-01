bdd0401236a526eff4f1c148d9127ef1a2dc927e
Anthonyhc18 <anherreraco@uide.edu.ec>
2026-06-01 15:05:24 +0000

    The Shebang Line

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+#!/usr/bin/env bash
 [Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ bash --version
 [Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ env
 [Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${SHELL}
 [Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${RANDOM}
 [Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${UID}
 [Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ echo ${OSTYPE}
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ ps -e -f
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ ps -ef
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ df --human-readable
+[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ df -h
+#[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 MUY BIEN✓ $ bash -x blackhatbash1.sh
+#[Anthony-Herrera] UNIX-02-SIN-C-Mar-Jul-2026 💀COJUDO💀✗ 127 $ bash -r blackhatbash1.sh
